1d07b3f01a4528dbf363fc188343775018e82079
mkn090 <mikkun090@gmail.com>
2016-03-17 18:58:12 +0900

    入力画面の実装

--- MyCalc/MyCalc/ViewController.swift
+++ MyCalc/MyCalc/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,85 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func tap1Button(sender: AnyObject) {
+         let value = priceField.text! + "1"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
 
+    @IBAction func tap2Button(sender: AnyObject) {
+        let value = priceField.text! + "2"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap3Button(sender: AnyObject) {
+        let value = priceField.text! + "3"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+
+    }
+    
+    @IBAction func tap4Button(sender: AnyObject) {
+        let value = priceField.text! + "4"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+
+    }
+    
+    @IBAction func tap5Button(sender: AnyObject) {
+        let value = priceField.text! + "5"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+
+    }
+    
+    @IBAction func tap6Button(sender: AnyObject) {
+        let value = priceField.text! + "6"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+
+    }
+    
+    @IBAction func tap7Button(sender: AnyObject) {
+        let value = priceField.text! + "7"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+
+    }
+    
+    @IBAction func tap8Button(sender: AnyObject) {
+        let value = priceField.text! + "8"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+
+    }
+    
+    @IBAction func tap9Button(sender: AnyObject) {
+        let value = priceField.text! + "9"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap00Button(sender: AnyObject) {
+        let value = priceField.text! + "00"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tapClearButton(sender: AnyObject) {
+        priceField.text = "0"
+    }
 }
 
