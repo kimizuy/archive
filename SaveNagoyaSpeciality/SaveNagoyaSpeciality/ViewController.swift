225bfeed25c4986a89bb904d9d0953a19b8e98f2
kimizuy <kimizuyamasaki@email.com>
2016-05-01 17:32:00 +0900

    Initial Commit

---
+++ SaveNagoyaSpeciality/SaveNagoyaSpeciality/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveNagoyaSpeciality
+//
+//  Created by Kimizu Yamasaki on 2016/05/01.
+//  Copyright © 2016年 Kimizu Yamasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
