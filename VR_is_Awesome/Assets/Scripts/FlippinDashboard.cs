fb52524b45727933e83bf9f50ff4cb42c4ef2775
kimizuy <kimizuyamasaki@email.com>
2016-11-28 23:07:53 +0900

    First Commit

---
+++ VR_is_Awesome/Assets/Scripts/FlippinDashboard.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlippinDashboard : MonoBehaviour {
+	private HeadGesture gesture;
+	private GameObject dashboard;
+	private bool isOpen = true;
+	private Vector3 startRotation;
+
+	// Use this for initialization
+	void Start () {
+		gesture = GetComponent<HeadGesture> ();
+		dashboard = GameObject.Find ("Dashboard");
+		startRotation = dashboard.transform.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gesture.isFacingDown) {
+			OpenDashboard ();
+		} else {
+			ClosedDashboard ();
+		}
+	}
+
+	private void ClosedDashboard(){
+		if (isOpen) {
+			dashboard.transform.eulerAngles = new Vector3 (180.0f, startRotation.y, startRotation.z);
+			isOpen = false;
+		}
+	}
+
+	private void OpenDashboard(){
+		if (!isOpen) {
+			dashboard.transform.eulerAngles = startRotation;
+			isOpen = true;
+		}
+	}
+}
