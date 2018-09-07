820517ebf6b8c5eb05ad15b15ed8a49e54f4dd5f
kimizuy <kimizuyamasaki@gmail.com>
2018-09-08 04:27:56 +0900

    Add mainButton

--- SampleAppWithoutStoryboard/SampleAppWithoutStoryboard/AppDelegate.swift
+++ SampleAppWithoutStoryboard/SampleAppWithoutStoryboard/AppDelegate.swift
@@ -17,6 +17,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         let mainTabBarController = MainTabBarController()
+        let mainNavigationController = MainNavigationController()
+        let secondaryViewController = SecondaryViewController()
+        let mainViewController = MainViewController()
+        
+        mainNavigationController.title = "NavVC"
+        secondaryViewController.title = "SecondaryVC"
+        mainViewController.title = "MainVC"
+        
+        mainNavigationController.viewControllers = [mainViewController]
+        
+        mainTabBarController.viewControllers = [mainNavigationController, secondaryViewController]
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
