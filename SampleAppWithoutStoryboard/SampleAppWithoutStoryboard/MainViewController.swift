4d487670379d1cac923a0f10fbeaa8f6023d9d1d
kimizuy <kimizuyamasaki@gmail.com>
2018-09-08 03:35:27 +0900

    Add some swift files

---
+++ SampleAppWithoutStoryboard/SampleAppWithoutStoryboard/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  SampleAppWithoutStoryboard
+//
+//  Created by ky on 2018-09-08.
+//  Copyright © 2018年 ky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
