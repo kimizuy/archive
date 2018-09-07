ae765093111535c9fe0f3a8d567dc32927f45c85
kimizuy <kimizuyamasaki@gmail.com>
2018-09-07 23:53:32 +0900

    Add MainTabBarController & MainNavigationController

---
+++ SampleAppWithoutStoryboard/SampleAppWithoutStoryboard/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  SampleAppWithoutStoryboard
+//
+//  Created by ky on 2018-09-07.
+//  Copyright © 2018年 ky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
