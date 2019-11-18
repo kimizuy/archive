09320de01fe773c7963560f633905a58cd9a9074
kimizuy <kimizuyamasaki@gmail.com>
2019-11-18 22:07:06 +0900

    chore: add storybook

--- react-playground/src/reducers/todos.js
+++ react-playground/src/reducers/todos.js
@@ -1,6 +1,6 @@
 const todos = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_TODO':
+    case "ADD_TODO":
       return [
         ...state,
         {
@@ -9,11 +9,9 @@ const todos = (state = [], action) => {
           completed: false
         }
       ]
-    case 'TOGGLE_TODO':
+    case "TOGGLE_TODO":
       return state.map(todo =>
-        (todo.id === action.id)
-          ? {...todo, completed: !todo.completed}
-          : todo
+        todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
       )
     default:
       return state
