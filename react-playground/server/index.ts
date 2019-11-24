63bb1efdfaeb3c522bdba6a6c0892851797a1767
kimizuy <kimizuyamasaki@gmail.com>
2019-11-24 18:59:01 +0900

    feat: add local server config

--- react-playground/server/index.ts
+++
@@ -1,6 +0,0 @@
-import express, { Request, Response } from 'express'
-const app = express()
-
-app.get('/', (_req: Request, res: Response) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
