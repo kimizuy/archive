6711a05ff918eaabd9e740f0c4d191e3be42e98e
kimizuy <kimizuyamasaki@gmail.com>
2020-01-26 19:35:11 +0900

    feat: body-parcer ミドルウェアを使ってフォームから取得した値を表示する

--- node-express-sample/app.js
+++ node-express-sample/app.js
@@ -2,16 +2,25 @@ var http = require('http')
 var path = require('path')
 var express = require('express')
 var morgan = require('morgan')
+var bodyParcer = require('body-parser')
 
 var app = express()
 
 app.use(morgan('combined'))
+app.use(bodyParcer.urlencoded({ extended: false }))
 
 app.set('views', path.join(__dirname, 'templates'))
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
   return res.render('index', { title: 'Hello World' })
+})
+
+app.post('/form', function(req, res) {
+  return res.render('result', {
+    username: req.body.username,
+    message: req.body.message
+  })
 })
 
 var server = http.createServer(app)
