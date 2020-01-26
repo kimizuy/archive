72b01cbc2e0e13c08a18c4aa9c43781c2608009e
kimizuy <kimizuyamasaki@gmail.com>
2020-01-26 19:21:49 +0900

    add template engine 'pug'

--- node-express-sample/app.js
+++ node-express-sample/app.js
@@ -1,11 +1,17 @@
 var http = require('http')
+var path = require('path')
 var express = require('express')
 var morgan = require('morgan')
 
 var app = express()
 
+app.use(morgan('combined'))
+
+app.set('views', path.join(__dirname, 'templates'))
+app.set('view engine', 'pug')
+
 app.get('/', function(req, res) {
-  return res.send('Hello World')
+  return res.render('index', { title: 'Hello World' })
 })
 
 var server = http.createServer(app)
